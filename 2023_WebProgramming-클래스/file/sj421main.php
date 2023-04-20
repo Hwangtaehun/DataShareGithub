ba6bc8279cb95e0a568a2c81f828cfb0b536799d
Hwangtaehun <r283@naver.com>
2023-04-20 19:59:54 +0900

    필요없는 파일 삭제

--- 2023_WebProgramming-클래스/file/sj421main.php
+++
@@ -1,6 +0,0 @@
-<?php
-    ob_start();
-    include 'sj421.inc.php';
-    $outString = ob_get_clean();
-    echo $outString;
-?>
--- 2023_WebProgramming-클래스/templates/stuName.html.php
+++
@@ -1,7 +0,0 @@
-<?php foreach ($stuName as $sName): ?>
-    <blockquote>
-        <p>
-        <?=$sName?>
-        </p>
-    </blockquote>
-<?php endforeach; ?>
