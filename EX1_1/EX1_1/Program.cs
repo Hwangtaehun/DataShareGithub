148b40c57d35cd1aa03ed541984f23e861c16a1e
Hwangtaehun <r283@naver.com>
2024-10-05 19:09:01 +0900

    Update Program.cs

--- EX1_1/EX1_1/Program.cs
+++ EX1_1/EX1_1/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace EX1_1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world");
+            Console.Write("Let me know your name: ");
+            String? name = Console.ReadLine();
+            Console.WriteLine("Hello, " + name);
+        }
+    }
+}
