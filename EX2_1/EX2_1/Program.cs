8df14c6999b3f54f69c31a5dc5a6e9b5a29829e9
Hwangtaehun <r283@naver.com>
2024-10-06 19:09:43 +0900

    업로드

---
+++ EX2_1/EX2_1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+int iVal = 30000;
+const Int32 cVal = 0X3ABC;
+
+iVal = iVal + cVal;
+
+Console.WriteLine(iVal);
+
+Console.WriteLine(int.MinValue);
+Console.WriteLine(UInt64.MinValue);
+Console.WriteLine(short.Parse("1234") + 4321);
+Console.WriteLine(cVal.ToString() + " *** " + 2000.ToString());
