809581a5c6a3e669dd0d56bbb37f834384e233f0
Hwangtaehun <r283@naver.com>
2024-10-14 19:22:14 +0900

    업로드

---
+++ EX7_1/Assembly/Parts/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ProcessingUnits
+{
+    public class CPU
+    {
+        public CPU() { }
+        public void getName()
+        {
+            Console.WriteLine("CPU");
+        }
+    }
+}
